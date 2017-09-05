6a4f99328357cd440fe8bde248cec27d4356a071
zephyr <zhangxin@quancheng-ec.com>
2017-09-05 11:46:57 +0800

    add setImmediate polyfill , uglify js bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
-const { resolve } = require('path')
+const webpack = require('webpack')
 
 module.exports = {
-  entry: './node_modules/cheerio/index.js',
+  entry: ['./node_modules/setimmediate/setImmediate.js', './node_modules/cheerio/index.js'],
   output: {
     path: __dirname,
     filename: 'index.js',
     libraryTarget: "commonjs"
-  }
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin()
+  ]
 }
